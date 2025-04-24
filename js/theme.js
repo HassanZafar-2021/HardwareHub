861594863254c50fed06f8d0dd71d038f08c307e
Your Name <your-email@example.com>
2025-04-24 01:01:07 -0400

    fixing alignment and coloring

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,16 @@
 function toggleTheme() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
+  const element = document.body;
+  const icon = document.getElementById("theme-icon");
+
+  const isDark = element.classList.toggle("dark-mode");
+  localStorage.theme = isDark ? "dark" : "light";
+
+  icon.src = isDark ? "../public/darkMode.svg" : "../public/lightMode.svg";
+}
+
+if (localStorage.theme === "dark") {
+  document.body.classList.add("dark-mode");
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("theme-icon").src = "../public/darkMode.svg";
+  });
 }
