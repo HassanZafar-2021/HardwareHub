a45c68ac4986caddbccbe557b7a35dfb35bf9fdb
Your Name <your-email@example.com>
2025-05-21 02:01:51 -0400

    Added almost working magnifying glass to images in home

--- js/home.js
+++ js/home.js
@@ -1,5 +1,8 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides(slideIndex);
+});
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
@@ -10,21 +13,26 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
   const slides = document.getElementsByClassName("mySlides");
   const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
+  const oldGlass = document.querySelector(".img-magnifier-glass");
+  if (oldGlass) oldGlass.remove();
+
+  const currentImg = slides[slideIndex - 1].querySelector("img");
+  if (currentImg && currentImg.id) magnify(currentImg.id, 2);
 }
